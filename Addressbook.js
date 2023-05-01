bf7dd043488b6ffb2a0d186c380937574294e1cd
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-05-01 14:51:07 +0530

    [sanjana] Add :AddressBook using js

---
+++ Addressbook.js
@@ -0,0 +1,29 @@
+class Contact
+{
+  firstname ;
+  lastname ;
+  address;
+  city;
+  state;
+  zip;
+  phone;
+  email;
+  constructor(  firstname ,lastname ,address,city,state, zip,phone,email)
+  {
+    this.firstname=firstname;
+    this.address=address;
+    this.lastname=lastname;
+    this.city=city;
+    this.state=state;
+    this.zip=zip;
+    this.phone=phone;
+    this.email=email;
+
+  }
+  tostring()
+  {
+    return "Firstname = "+this.firstname +" Lasttname = "+this.lastname +" City = "+this.city +" State = "+this.state +" ZipCode = "+this.zip +" Phone Number = "+this.phone + " Email = "+this.email;
+  }
+}
+contact = new Contact("Sanjana","Dhenge","1461","Kop","MH","416012","9359545565","abc@gmail.com");
+console.log(contact.tostring());
